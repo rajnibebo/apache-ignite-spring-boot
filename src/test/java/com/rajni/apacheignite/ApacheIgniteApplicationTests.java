4676225f3be7c2e31e140644f43279c58d53aa10
rajniubhi <ubhi.rajni800@gmail.com>
2021-11-26 23:35:41 +0530

    Spring boot setup for the project

---
+++ src/test/java/com/rajni/apacheignite/ApacheIgniteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajni.apacheignite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheIgniteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
